bf87f1a34e387cd477558ff7f6f73f53e30d9854
M. Kerschner <moises.kerschner@unochapeco.edu.br>
2026-06-09 01:54:41 -0300

    4.1: primeng toast added to app

--- frontend/src/app/pages/dashboard/dashboard.page.ts
+++ frontend/src/app/pages/dashboard/dashboard.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Toast } from 'primeng/toast';
 import { ErrorListContainer } from '../../containers/error-list/error-list.container';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ErrorListContainer],
+  imports: [Toast, ErrorListContainer],
   templateUrl: './dashboard.page.html'
 })
 export class DashboardPage {}
